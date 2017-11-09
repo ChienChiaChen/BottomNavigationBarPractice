a231b075701b6f5045d80f3202bad0acb0622bc0
JasonChien <s124202468@gmail.com>
2017-11-10 05:37:21 +0800

    Setup fragment

---
+++ app/src/main/java/com/chiachen/bottomnavigationbarpractice/ItemOneFragment.java
@@ -0,0 +1,29 @@
+package com.chiachen.bottomnavigationbarpractice;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by jianjiacheng on 10/11/2017.
+ */
+
+public class ItemOneFragment extends Fragment {
+    public static Fragment newInstance() {
+        return new ItemOneFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_item_one, container, false);
+    }
+}
