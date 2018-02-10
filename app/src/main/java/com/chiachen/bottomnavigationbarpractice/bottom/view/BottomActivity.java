2ce1218216836aa9a88f3b0d61d160ae1afc8145
JasonChien <s124202468@gmail.com>
2018-02-10 14:18:41 +0800

    Add fragments.

--- app/src/main/java/com/chiachen/bottomnavigationbarpractice/bottom/view/BottomActivity.java
+++ app/src/main/java/com/chiachen/bottomnavigationbarpractice/bottom/view/BottomActivity.java
@@ -1,9 +1,19 @@
 package com.chiachen.bottomnavigationbarpractice.bottom.view;
 
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
+import com.chiachen.bottomnavigationbarpractice.ItemFiveFragment;
+import com.chiachen.bottomnavigationbarpractice.ItemFourFragment;
+import com.chiachen.bottomnavigationbarpractice.ItemSixFragment;
+import com.chiachen.bottomnavigationbarpractice.ItemThreeFragment;
+import com.chiachen.bottomnavigationbarpractice.ItemTwoFragment;
 import com.chiachen.bottomnavigationbarpractice.R;
 import com.chiachen.bottomnavigationbarpractice.bottom.presenter.BottomPresenter;
 import com.chiachen.bottomnavigationbarpractice.bottom.presenter.IBottomPresenter;
@@ -11,36 +21,48 @@ import com.chiachen.bottomnavigationbarpractice.bottom.presenter.IBottomPresente
 public class BottomActivity extends AppCompatActivity implements IBottomView {
 
     private IBottomPresenter mPresenter;
+    FragmentManager fm;
+    private Fragment mFragmentNow;
+    
     private View.OnClickListener mOnTabClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             mPresenter.clearTabStatus();
+            Fragment selectedFragment = null;
             switch (v.getId()) {
                 case R.id.bottom_tab_diary:{
                     getDiary().setSelected(true);
+                    selectedFragment = ItemTwoFragment.newInstance();
                     break;
                 }
 
                 case R.id.bottom_tab_report:{
                     getReport().setSelected(true);
+                    selectedFragment = ItemThreeFragment.newInstance();
                     break;
                 }
 
                 case R.id.bottom_tab_camera:{
                     getCamera().setSelected(true);
+                    selectedFragment = ItemFourFragment.newInstance();
                     break;
                 }
 
                 case R.id.bottom_tab_coach:{
                     getCoach().setSelected(true);
+                    selectedFragment = ItemFiveFragment.newInstance();
                     break;
                 }
 
                 case R.id.bottom_tab_more:{
                     getMore().setSelected(true);
+                    selectedFragment = ItemSixFragment.newInstance();
                     break;
                 }
             }
+
+            if (null == selectedFragment) return;
+            switchContent(mFragmentNow,selectedFragment);
         }
     };
 
@@ -61,6 +83,15 @@ public class BottomActivity extends AppCompatActivity implements IBottomView {
         getCamera().setOnClickListener(mOnTabClickListener);
         getCoach().setOnClickListener(mOnTabClickListener);
         getMore().setOnClickListener(mOnTabClickListener);
+        setDefaultFragment();
+    }
+
+    private void setDefaultFragment() {
+        fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        mFragmentNow = ItemTwoFragment.newInstance();
+        transaction.add(R.id.frame_layout, mFragmentNow);
+        transaction.commit();
     }
 
     @Override
@@ -90,5 +121,21 @@ public class BottomActivity extends AppCompatActivity implements IBottomView {
 
     private View getDiary() {
         return findViewById(R.id.bottom_tab_diary);
+    }
+
+    private void switchContent(Fragment from, Fragment to) {
+        if (mFragmentNow == to) {
+            return;
+        }
+
+        mFragmentNow = to;
+        FragmentTransaction transaction = fm.beginTransaction();
+        if (!to.isAdded()) {    // 先判断是否被add过
+            Log.e("JASON_CHIEN", "\nis Not Added");
+            transaction.hide(from).add(R.id.frame_layout, to).commit(); // 隐藏当前的fragment，add下一个到Activity中
+        } else {
+            Log.e("JASON_CHIEN", "\nis Added");
+            transaction.hide(from).show(to).commit(); // 隐藏当前的fragment，显示下一个
+        }
     }
 }
