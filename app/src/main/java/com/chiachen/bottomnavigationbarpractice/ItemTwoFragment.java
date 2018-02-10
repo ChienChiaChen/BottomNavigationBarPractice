3eb590d23977b6b30a19cee914e8df4698c0e2bc
JasonChien <s124202468@gmail.com>
2018-02-10 14:15:14 +0800

    Add log for lifecycle.

--- app/src/main/java/com/chiachen/bottomnavigationbarpractice/ItemTwoFragment.java
+++ app/src/main/java/com/chiachen/bottomnavigationbarpractice/ItemTwoFragment.java
@@ -3,6 +3,7 @@ package com.chiachen.bottomnavigationbarpractice;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,12 +20,38 @@ public class ItemTwoFragment extends Fragment {
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
+        Log.e("JASON_CHIEN", "\nonCreate");
         super.onCreate(savedInstanceState);
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.e("JASON_CHIEN", "\nonCreateView");
         return inflater.inflate(R.layout.fragment_item_two, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Log.e("JASON_CHIEN", "\nonViewCreated");
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.e("JASON_CHIEN", "\nonActivityCreated");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.e("JASON_CHIEN", "\nonStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.e("JASON_CHIEN", "\nonResume");
     }
 }
